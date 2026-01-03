9816482a814933f765e810d60361d2e9ee6b5cca
AwaisSDev <mawais9171@gmail.com>
2026-01-03 16:47:55 +0500

    Connected to the Database

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lpxhedhqdakigtvqeppq.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxweGhlZGhxZGFraWd0dnFlcHBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0MzY3NDcsImV4cCI6MjA4MzAxMjc0N30.gyWf_jGYz7wNViCiCcrKoxXeUN0Re3OSmdjz4XMs_Hw'
+const supabaseUrl = 'YOUR_SUPABASE_URL'
+const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
