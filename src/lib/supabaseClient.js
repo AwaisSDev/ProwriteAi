f37b71bc4a3e21c50e02685284dc3d6eb9b1f6a4
AwaisSDev <mawais9171@gmail.com>
2026-01-07 23:46:55 +0500

    Commented

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // These pull from your .env file locally or Vercel settings online
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL // The name of env var is VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY // The name of env var is VITE_SUPABASE_ANON_KEY
 if (!supabaseUrl || !supabaseAnonKey) {
     console.error("Missing Supabase Environment Variables!")
 }
